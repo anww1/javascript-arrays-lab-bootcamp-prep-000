e7c668359977abfd13e3f7b06f1eb62b9c7a5e01
Alexandria Walker-Wade <alexandriawalkerwade@gmail.com>
2018-11-28 20:53:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,65 @@
-var kittens = [] //define your array here
+var kittens = ["Milo", "Otis", "Garfield"];
 
-// Add your functions and code here
+
+function destructivelyAppendKitten(name) 
+
+{
+kittens.push("Ralph");
+}
+  
+function destructivelyPrependKitten(name) 
+
+{
+kittens.unshift("Bob");
+}
+
+
+function destructivelyRemoveLastKitten(name) 
+
+{
+kittens.pop("Garfield");
+}
+
+
+
+function destructivelyRemoveFirstKitten(name) 
+
+{
+kittens.shift("Milo");
+}
+
+
+
+function appendKitten(name)
+{
+  var newArray = kittens.slice();
+  
+  newArray.push("Broom")
+  return newArray
+
+}
+
+function prependKitten(name)
+{
+
+var newArray = ["Arnold","Milo", "Otis", "Garfield"];
+return newArray
+}
+
+function removeLastKitten()
+{
+var newArray = ["Milo", "Otis"];
+
+return newArray;
+}
+
+
+
+function removeFirstKitten(name)
+{
+  
+var newArray = ["Otis", "Garfield"];
+
+return newArray;
+
+}
